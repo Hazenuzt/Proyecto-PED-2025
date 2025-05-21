6ac66b8e0e19d918e2d3ca32385daea502c97858
mendexzx <pp734148@gmail.com>
2025-05-20 21:14:11 -0600

    using .db

--- Proyecto-PED/Proyecto-PED/Controlador/Controlador.cs
+++ Proyecto-PED/Proyecto-PED/Controlador/Controlador.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Proyecto_PED.Modelo;
+using Proyecto_PED.Modelo.BD;
 
 namespace Proyecto_PED.Controlador
 {
