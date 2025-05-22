c994c958c1619f15e18fbca8ebe35de16f09b54a
mendexzx <pp734148@gmail.com>
2025-05-21 18:30:53 -0600

    arreglitos

--- Proyecto-PED/Proyecto-PED/Modelo/Entidades/Usuario.cs
+++ Proyecto-PED/Proyecto-PED/Modelo/Entidades/Usuario.cs
@@ -24,7 +24,7 @@ namespace Proyecto_PED.Modelo.Entidades
         // para el imc 
         public double imcCalcular()
         {
-            if (estatura <= 0) return 0;
+            if (estatura <= 0) return 0;//evitamos deviviones entre cero
             return peso / (estatura * estatura);
         }
 
